5d63f7d65b4631cbe07a2cb01a53a769d50f17a1
Edward Chung <edwardchung2024@gmail.com>
2022-08-22 04:26:05 -0400

    updatedPost route added

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js';
 
 // separating all functions in routes
@@ -24,4 +25,17 @@ export const createPost = async (req, res) => {
     } catch (error) {
         res.status(409).json({ message: error.message })
     }
+}
+
+// /posts/123 <--fills id value 
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body; //<--sent from front end
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+    
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new : true });
+
+    res.json(updatedPost);
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
 //need to add .js at the end in node (not in react)
-import { getPosts, createPost } from '../controllers/posts.js'
+import { getPosts, createPost, updatePost} from '../controllers/posts.js'
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
+router.patch('/:id', updatePost);
 
 export default router;
