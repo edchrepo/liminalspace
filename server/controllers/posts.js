ce212d1b6d81ea50d002b514ed95afde001c1fa4
Edward Chung <edwardchung2024@gmail.com>
2022-08-23 11:49:33 -0400

    delete post added

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -39,3 +39,15 @@ export const updatePost = async (req, res) => {
 
     res.json(updatedPost);
 }
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    //check if id is valid
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: 'Post deleted successfully' });
+
+}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,12 +1,13 @@
 import express from 'express';
 
 //need to add .js at the end in node (not in react)
-import { getPosts, createPost, updatePost} from '../controllers/posts.js'
+import { getPosts, createPost, updatePost, deletePost } from '../controllers/posts.js'
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
 
 export default router;
