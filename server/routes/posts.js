60463dc76f8a27e4f07422fa5de1b30ce02fdeaf
Edward Chung <edwardchung2024@gmail.com>
2022-08-31 01:30:53 -0400

    like post implemented

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 //need to add .js at the end in node (not in react)
-import { getPosts, createPost, updatePost, deletePost } from '../controllers/posts.js'
+import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js'
 
 const router = express.Router();
 
@@ -9,5 +9,6 @@ router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
 router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
 
 export default router;
