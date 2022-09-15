18042bf58f79128e114e579f85be3e89275af704
Edward Chung <edwardchung2024@gmail.com>
2022-09-15 02:56:33 -0400

    auth middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,6 +5,7 @@ import jwt from 'jsonwebtoken'
 const auth = async (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
+        console.log(`this is the token: ${token}`)
         const isCustomAuth = token.length < 500;
 
         let decodedData; 
