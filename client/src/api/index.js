07b69282094dd522022c6724893a47cf87db2eac
Edward Chung <edwardchung2024@gmail.com>
2022-09-21 01:31:55 -0400

    pagination front end

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -14,7 +14,7 @@ API.interceptors.request.use((req) => {
 })
 
 //sending to endposts ex:localhost:300/posts
-export const fetchPosts = () => API.get('/posts');
+export const fetchPosts = (page) => API.get(`/posts?page=${page}`);
 export const fetchPostsBySearch = (searchQuery) => API.get(`/posts/search?searchQuery=${searchQuery.search || 'none'}&tags=${searchQuery.tags}`) 
 export const createPost = (newPost) => API.post('/posts', newPost);
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -27,10 +27,6 @@ const Home = () => {
     const [search, setSearch] = useState('');
     const [tags, setTags] = useState([]);
 
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [currentId, dispatch]);
-
     const searchPost = () => {
         if(search.trim() || tags) {
             // [europe, usa] -> 'europe, usa' for tags.join
@@ -82,7 +78,7 @@ const Home = () => {
                     </AppBar>
                         <Form currentId={currentId} setCurrentId={setCurrentId} />
                         <Paper elevation={6}>
-                            <Pagination />
+                            <Pagination page={page} />
                         </Paper>
                     </Grid>
                 </Grid>
