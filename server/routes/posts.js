075b705b856faf8183ed97a6f46e2146a36388ca
Edward Chung <edwardchung2024@gmail.com>
2022-09-21 16:37:14 -0400

    details page backend

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,13 +1,15 @@
 import express from 'express';
 
 //need to add .js at the end in node (not in react)
-import { getPostsBySearch, getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js'
+import { getPostsBySearch, getPosts, getPost, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js'
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/search', getPostsBySearch);
 router.get('/', getPosts);
+router.get('/:id', getPost);
+
 router.post('/', auth, createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
