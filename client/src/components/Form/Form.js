adc42b157371655d98e7f232474d2f355a280258
Edward Chung <edwardchung2024@gmail.com>
2022-09-25 19:32:35 -0400

    minor elevation change

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -43,7 +43,7 @@ const Form = ({ currentId, setCurrentId }) => {
 
     if(!user?.result?.given_name) {
         return (
-            <Paper className={classes.paper}>
+            <Paper className={classes.paper} elevation={6}>
                 <Typography variant="h6" align="center">
                     Please Sign In to add your Liminal Space and like other's Liminal Spaces.
                 </Typography>
